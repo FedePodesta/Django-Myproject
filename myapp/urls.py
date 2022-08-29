9821885ef587bd3827c1d2e65c2ca8b34d60f342
FedePodesta <federicoandrespodesta@gmail.com>
2022-08-29 20:34:48 -0300

    Clase2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
-    path("acerca-de",views.informar,name="acerca")
+    path("", views.index, name="index"),
+    path("acerca-de", views.acerca_de, name="acerca_de"),
+    path("cursos", views.cursos, name="cursos"),
+    path("cursos/json", views.cursos_json, name="cursos_json"),
+    path("cotizacion-dolar", views.cotizacion_dolar, name="cotizacion_dolar"),
+    path("aeropuertos", views.aeropuertos, name="aeropuertos"),
+    path("aeropuertos/json", views.aeropuertos_json, name="aeropuertos_json")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,107 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+import sqlite3
+import requests
+from django.http import HttpResponse, JsonResponse
 
-def index (request):
-    return HttpResponse ("¡Hola, mundo!")
 
-def informar (request):
-    return HttpResponse ("Esta es nuestra primera web realizada con DJango 2022")
+def index(request):
+    return HttpResponse("<html>¡<strong>Hola</strong>, <em>mundo</em>!</html>")
+
+
+def acerca_de(request):
+    return HttpResponse("¡Curso de Python y Django!")
+
+
+def cursos_json(request):
+    conn = sqlite3.connect("cursos.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT nombre, inscriptos FROM cursos")
+    response = JsonResponse(cursor.fetchall(), safe=False)
+    conn.close()
+    return response
+
+
+def cursos(request):
+    conn = sqlite3.connect("cursos.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT nombre, inscriptos FROM cursos")
+    html = """
+        <html>
+        <title>Lista de cursos</title>
+        <table style="border: 1px solid">
+          <thead>
+            <tr>
+              <th>Curso</th>
+              <th>Inscriptos</th>
+            </tr>
+          </thead>
+    """
+    for (nombre, inscriptos) in cursor.fetchall():
+        html += f"""
+            <tr>
+              <td>{nombre}</td>
+              <td>{inscriptos}</td>
+            </tr>
+        """
+    html += "</table></html>"
+    conn.close()
+    return HttpResponse(html)
+
+
+def cotizacion_dolar(request):
+    r = requests.get("https://api.recursospython.com/dollar")
+    resultado = r.json()
+    html = f"""
+        <html>
+        <title>Cotización del dólar</title>
+        <p><strong>Compra</strong>: {resultado["buy_price"]}</p>
+        <p><strong>Venta</strong>: {resultado["sale_price"]}</p>
+        </html>
+    """
+    return HttpResponse(html)
+
+
+
+def aeropuertos(request):
+    f = open("aeropuertos.csv", encoding="utf8")
+    html = """
+        <html>
+        <title>Lista de aeropuertos</title>
+        <table style="border: 1px solid">
+          <thead>
+            <tr>
+              <th>Aeropuerto</th>
+              <th>Ciudad</th>
+              <th>País</th>
+            </tr>
+          </thead>
+    """
+    for linea in f:
+        datos = linea.split(",")
+        nombre = datos[1].replace('"', "")
+        ciudad = datos[2].replace('"', "")
+        pais = datos[3].replace('"', "")
+        html += f"""
+            <tr>
+              <td>{nombre}</td>
+              <td>{ciudad}</td>
+              <td>{pais}</td>
+            </tr>
+        """
+    f.close()
+    html += "</table></html>"
+    return HttpResponse(html)
+
+
+def aeropuertos_json(request):
+    f = open("aeropuertos.csv", encoding="utf8")
+    aeropuertos = []
+    for linea in f:
+        datos = linea.split(",")
+        aeropuerto = {
+            "nombre": datos[1].replace('"', ""),
+            "ciudad": datos[2].replace('"', ""),
+            "pais": datos[3].replace('"', "")
+        } 
+        aeropuertos.append(aeropuerto)
+    f.close()
+    return JsonResponse(aeropuertos, safe=False)
